228ea77643c86203ac97891a486fa0bc2add1482
arxsolid <arxsolid@penguin>
2019-05-14 14:33:32 +0900

    いらないパッケージを削除

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: './src/popup.ts',
-  // mode: "development",
   devtool: 'cheap-module-source-map',
   resolve: {
     extensions: [ '.tsx', '.ts', '.js'],
