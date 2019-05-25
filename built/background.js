0c1a63a6e67a825126b8f5082d7eff3e823da7b3
arxsolid <arxsolid@penguin>
2019-05-25 16:31:45 +0900

    new tab pageでextensionを開いた場合、専用のurlにリダイレクトする

--- built/background.js
+++ built/background.js
@@ -1,10 +1,15 @@
-
 let currentTab
-
+const log = console.log
 // extensionボタンが押されたらcontent scriptsにメッセージ
 chrome.browserAction.onClicked.addListener( tab => {
   currentTab = tab
-  chrome.tabs.sendMessage(tab.id, {})
+  if (tab.url === 'chrome://newtab/') { // 現在のタブがnew tabならリダイレクトする
+    chrome.tabs.create({
+      url: chrome.extension.getURL('view.html')
+    })
+  } else {
+    chrome.tabs.sendMessage(tab.id, {})
+  }
 })
 
 chrome.tabs.onActivated.addListener(() => {
